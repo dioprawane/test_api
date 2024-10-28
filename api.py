cef1a1c98e18b4910624703f66caccbb0104d89c
Diop Serigne Rawane <serigne-rawane.diop@etu.unice.fr>
2024-10-28 08:57:42 +0100

    Supprime le fichier mon_modele_bis.h5 du suivi Git

--- api.py
+++ api.py
@@ -5,9 +5,12 @@ from PIL import Image
 import numpy as np
 import base64
 import io
+import requests
+import os
+import tempfile
 
-# Charger le modèle
-model = load_model('mon_modele_bis.h5')
+# URL de téléchargement direct OneDrive (modifiée)
+model_url = "https://onedrive.live.com/download?cid=0D4B5A54C6A73CFF&resid=0D4B5A54C6A73CFF%211261&authkey=ANuKYnFiJe_wUbA"
 
 # Définir les noms de classes
 class_names = {0: 'bottomwear', 1: 'shoes', 2: 'topwear'}
@@ -26,6 +29,17 @@ async def predict(file: UploadFile = File(...)):
     if file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Format de fichier non pris en charge. Utilisez JPEG ou PNG.")
     
+    # Téléchargement temporaire du modèle depuis OneDrive
+    with tempfile.NamedTemporaryFile(suffix=".h5", delete=False) as temp_model_file:
+        response = requests.get(model_url)
+        if response.status_code == 200:
+            temp_model_file.write(response.content)
+        else:
+            raise HTTPException(status_code=500, detail="Erreur lors du téléchargement du modèle.")
+
+    # Charger le modèle téléchargé temporairement
+    model = load_model(temp_model_file.name)
+
     # Charger et prétraiter l'image
     image = Image.open(file.file).convert("RGB")
     image = image.resize((64, 64))  # Adapter à la taille d'entrée du modèle
@@ -42,11 +56,11 @@ async def predict(file: UploadFile = File(...)):
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
     
+    # Supprimer le fichier modèle temporaire
+    os.remove(temp_model_file.name)
+    
     return {
         "predicted_class": pred_class,
         "predicted_probability": float(np.max(pred_probs)),
         "image_base64": img_str
     }
-
-# Lancer l'application avec Uvicorn
-# Commande pour lancer l'application : uvicorn nom_du_script:app --reload
